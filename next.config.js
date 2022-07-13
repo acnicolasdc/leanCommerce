7f5626baf6e248f4dca3c32a85c6a5b984dcd468
Saray Bernat Bejarano <saraybernatbejarano@MacBook-Air-de-Saray.local>
2022-07-13 13:57:05 +0200

    nextjs global variables

--- next.config.js
+++ next.config.js
@@ -2,13 +2,17 @@ const { i18n } = require("./next-i18next.config");
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 module.exports = withPWA({
-	pwa: {
-		disable: process.env.NODE_ENV !== "production",
-		dest: "public",
-		runtimeCaching,
-	},
-	i18n,
-	typescript: {
+  pwa: {
+    disable: process.env.NODE_ENV !== "production",
+    dest: "public",
+    runtimeCaching,
+  },
+  env: {
+    NEXT_PUBLIC_REST_API_ENDPOINT: process.env.NEXT_PUBLIC_REST_API_ENDPOINT,
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+  },
+  i18n,
+  typescript: {
     ignoreBuildErrors: true,
   },
 });
